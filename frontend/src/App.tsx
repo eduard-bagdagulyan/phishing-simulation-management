ec9dd9eca86687210da7406688dc7ea546aad998
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2024-12-15 17:26:34 +0400

    [frontend] implement react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import PhishingPage from './pages/PhishingPage';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>{import.meta.env.VITE_API_URL}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/" element={<PhishingPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
