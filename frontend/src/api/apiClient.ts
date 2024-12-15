11941d199ef716abc64d24b41d5375b0ca21fc29
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2024-12-15 16:31:30 +0400

    [frontend] implement apiClient

---
+++ frontend/src/api/apiClient.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: `/api`,
+});
+
+export function setAuthToken(token: string | null) {
+  if (token) {
+    apiClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete apiClient.defaults.headers.common['Authorization'];
+  }
+}
+
+export default apiClient;
