45b2e89bfcc3b9912a86e74bc2f0ad1a4737aab0
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2024-12-15 13:53:45 +0400

    [phishing-simulation] config ValidationPipe and ConfigModule

---
+++ backend/phishing-simulation/src/common/env.validation.ts
@@ -0,0 +1,23 @@
+import { plainToInstance } from 'class-transformer';
+import { IsNumber, Max, Min, validateSync } from 'class-validator';
+
+class EnvironmentVariables {
+  @IsNumber()
+  @Min(0)
+  @Max(65535)
+  APP_PORT: number;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
--- backend/phishing-simulation/src/main.ts
+++ backend/phishing-simulation/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.APP_PORT);
 }
-bootstrap();
+
+bootstrap().then(() =>
+  console.log(`Server is running on port: ${process.env.APP_PORT}`),
+);
