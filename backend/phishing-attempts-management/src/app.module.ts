fcf2119b012c6c1790a22804978740e0132e7c67
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2024-12-15 14:41:54 +0400

    [phishing-attempts-management] MongooseModule setup

--- backend/phishing-attempts-management/src/app.module.ts
+++ backend/phishing-attempts-management/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './common/env.validation';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { validate } from './common/env.validation';
       envFilePath: '../../.env',
       validate,
     }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
   ],
 })
 export class AppModule {}
